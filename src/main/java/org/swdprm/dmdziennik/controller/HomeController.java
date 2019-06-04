9e9e3987d3fdb185fe3f1900583622f04ceead92
leszekjanczewski <leszek.janczewski@gmail.com>
2019-06-04 22:32:39 +0200

    changes needed to work in my computer

--- src/main/java/org/swdprm/dmdziennik/controller/HomeController.java
+++ src/main/java/org/swdprm/dmdziennik/controller/HomeController.java
@@ -1,10 +1,19 @@
 package org.swdprm.dmdziennik.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.swdprm.dmdziennik.model.User;
+import org.swdprm.dmdziennik.service.UserService;
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    UserService userService;
 
     @GetMapping("/")
     public String root() {
@@ -17,8 +26,15 @@ public class HomeController {
     }
 
     @GetMapping("/login")
-    public String login() {
-        return "login";
+    public ModelAndView login() {
+        ModelAndView modelAndView = new ModelAndView();
+/*
+        Authentication principal = SecurityContextHolder.getContext().getAuthentication();
+        User user = userService.findByUserLogin(principal.getName());
+        modelAndView.addObject("user", user);
+*/
+        modelAndView.setViewName("login");
+        return modelAndView;
     }
 
     @GetMapping("/access-denied")
